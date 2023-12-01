7ed061ce96bc0faa2deaef7fcb9d158741dd6974
guimarte <guimartinsstyle@gmail.com>
2023-12-01 11:24:27 -0300

    https

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  String url = "http://api.pokercup.app:3000";
+  String url = "https://api.pokercup.app:3000";
 
   Dio dio = Dio();
 }
